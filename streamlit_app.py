82a317074ce297f438e8a1f2e5dfe6f67e3f8b7f
y-yasuda0710 <yasudariku0710@gmail.com>
2024-10-24 05:01:42 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,17 @@ import streamlit as st
 st.set_page_config(page_title="タイトル", layout="wide")
 
 # タイトルを設定
-st.title('Streamlitのサンプルアプリ')
+st.title('ジャグラー')
 
 # テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
-user_input = st.text_input('あなたの名前を入力してください')
+user_input = st.text_input('あなたの借金額を入力してください')
 
 # ボタンを作成し、クリックされたらメッセージを表示
-if st.button('挨拶する'):
+if st.button('借金！！！'):
     if user_input:  # 名前が入力されているかチェック
-        st.success(f'🌟 こんにちは、{user_input}さん、いっぱい出たね! 🌟')  # メッセージをハイライト
+        st.success(f'🌟 あなたの借金は{user_input}円です!ジャグラーで一攫千金目指しましょう!!! 🌟')  # メッセージをハイライト
     else:
-        st.error('名前を入力してください。')  # エラーメッセージを表示
+        st.error('借金を入力してください。')  # エラーメッセージを表示
 
 # スライダーを作成し、値を選択
 number = st.slider('好きな数字（10進数）を選んでください', 0, 100)
@@ -29,3 +29,5 @@ st.write(f'あなたが選んだ数字は「{number}」です。')
 # 選択した数値を2進数に変換
 binary_representation = bin(number)[2:]  # 'bin'関数で2進数に変換し、先頭の'0b'を取り除く
 st.info(f'🔢 10進数の「{number}」を2進数で表現すると「{binary_representation}」になります。 🔢')  # 2進数の表示をハイライト
+
+min_
